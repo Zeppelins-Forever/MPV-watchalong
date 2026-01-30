7c38624d10966c2864914bd562b54a1aa70c0a92
Zeppelins-Forever <128062431+Zeppelins-Forever@users.noreply.github.com>
2026-01-30 16:28:37 -0600

    Include locale.h and set LC_NUMERIC to 'C'
    Added locale header and set numeric locale to 'C' for mpv compatibility.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <locale.h> // <--- 1. Add this include
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    // <--- 2. Add this line.
+    // Qt sets the locale to the system default, which breaks mpv.
+    // We must reset the numeric handling to "C" (standard) for mpv to load.
+    setlocale(LC_NUMERIC, "C");
+
     MainWindow w;
     w.show();
     return a.exec();
